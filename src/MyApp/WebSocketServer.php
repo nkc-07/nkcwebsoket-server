241f4f3da615b6e53d76de2873a4856a701f04dd
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-10-30 12:28:04 +0900

    チャット用の返す値のみを追加

--- src/MyApp/WebSocketServer.php
+++ src/MyApp/WebSocketServer.php
@@ -2,20 +2,20 @@
 namespace MyApp;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
- 
+
 class WebSocketServer implements MessageComponentInterface {
     protected $clients;
- 
+
     public function __construct() {
         $this->clients = new \SplObjectStorage;
     }
- 
+
     public function onOpen(ConnectionInterface $conn) {
         $this->clients->attach($conn);
 
         echo "New connection! ({$conn->resourceId})\n";
     }
- 
+
     public function onMessage(ConnectionInterface $from, $msg) {
         $from_param = $this->parse_url_param($from->httpRequest->getRequestTarget());
         $msgObject = json_decode($msg, true);
@@ -49,21 +49,53 @@ class WebSocketServer implements MessageComponentInterface {
                     $client->send(json_encode($json_object['data']));
                 }
             }
-        }
+        } else if($from_param['mode'] === 'chat') {
+            // $context = stream_context_create(
+            //     array(
+            //         'http' => array(
+            //             'method'=> 'POST',
+            //             'header'=> 'Content-type: application/json; charset=UTF-8',
+            //             'content' => http_build_query($msgObject)
+            //         )
+            //     )
+            // );
+            // $json_object = json_decode(
+            //     file_get_contents(
+            //         'http://localhost:8080/api/group/groupchat.php',
+            //         false,
+            //         $context
+            //     ),
+            //     true
+            // );
+            $json_object = [
+                'nickname' => 'test user',
+                'icon' => 'test',
+                'message' => 'test message'
+            ];
 
-        // TODO: チャット用処理
+            foreach ($this->clients as $client) {
+                $client_parm = $this->parse_url_param($client->httpRequest->getRequestTarget());
+                if (
+                        $from !== $client &&
+                        $from_param['mode'] === $client_parm['mode'] &&
+                        $from_param['room'] == $client_parm['room']
+                    ) {
+                    $client->send(json_encode($json_object));
+                }
+            }
+        }
     }
- 
+
     public function onClose(ConnectionInterface $conn) {
- 
+
         $this->clients->detach($conn);
- 
+
         echo "Connection {$conn->resourceId} has disconnected\n";
     }
- 
+
     public function onError(ConnectionInterface $conn, \Exception $e) {
         echo "An error has occurred: {$e->getMessage()}\n";
- 
+
         $conn->close();
     }
 
